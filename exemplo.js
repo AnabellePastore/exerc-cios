0cf43318111a86cec61350e287b9e28c9776ee0e
AnabellePastore <42332170812@info.local>
2024-05-02 17:00:31 -0300

    feat: material de início

---
+++ exemplo.js
@@ -0,0 +1,23 @@
+//prompt: recebe uma entrada digitada
+
+let nome = prompt("ola, digite seu nome: ")
+
+//concatenação 
+console.log("seja bem vindo " + nome + "!!!");
+
+let pontos = 100
+
+//parseInt: transforma oq está entre () em numero inteiro
+let bonus = parseInt (prompt("quantos pontos vc deseja somar?"))
+
+//adição -> num1 + num2
+//subtração -> num1 - num2
+//Multiplicação -> num1 * num2
+//divisão -> num1 / num2
+
+
+//console.log(pontos + bonus)
+
+//parseFloat(): transforma oq esta entre () em numero com , (quabrado)
+let notaAluno = parseFloat(prompt("digite a nome do aluno"))
+
