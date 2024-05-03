60fac05c406edc70cfdf447cb753ae9340919030
AnabellePastore <42332170812@info.local>
2024-05-03 17:08:47 -0300

    feat: exercício 5 feito

---
+++ ex5.js
@@ -0,0 +1,16 @@
+//Exercício 5: Verificação de números em ordem crescente
+//Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
+//crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou não.
+
+let n1= parseFloat(prompt ("Informe o primeiro numero de 1 a 3: ")) ;
+
+let n2= parseFloat(prompt ("Informe o segundo numero de 1 a 3: ")) ;
+
+let n3= parseFloat(prompt ("Informe o terceiro numero de 1 a 3: ")) ;
+
+if (n1 > n2 > n3){
+    console.log( "os números estçao em ordem crescente")
+}
+else{
+    console.log("os números não estão em ordem crescente")
+}
