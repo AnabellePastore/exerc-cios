6834df47f148e8a828afd7175070a6b6a13367b7
AnabellePastore <42332170812@info.local>
2024-05-03 17:18:13 -0300

    feat: exercício 6 feito

---
+++ ex6.js
@@ -0,0 +1,16 @@
+//Exercício 6: Verificação de idade
+//Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+//de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let year = prompt("digite seu ano de nascimento") 
+
+let calc = 2024 - year
+
+if(calc >= 18){
+    console.log("você é maior de idade")
+
+}
+
+else{
+    console.log("você não é maior de idade, saia deste site!!!")
+}
