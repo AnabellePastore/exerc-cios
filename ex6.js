b513fd29f4daccb6537e006609c7e009b3ad5a5e
AnabellePastore <42332170812@info.local>
2024-05-06 15:25:53 -0300

    feat: exercício 6 feito

--- ex6.js
+++ ex6.js
@@ -2,7 +2,7 @@
 //Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
 //de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
 
-let year = prompt("digite seu ano de nascimento") 
+let year =  parseInt (prompt("digite seu ano de nascimento")) 
 
 let calc = 2024 - year
 
