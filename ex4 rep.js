22b01a519dc7b5fd84bbc82c537326feafa58473
AnabellePastore <42332170812@info.local>
2024-05-08 15:47:12 -0300

    feat: exercício 4 feito

---
+++ ex4 rep.js
@@ -0,0 +1,12 @@
+//4. Faça um programa que imprima na tela a tabuada de um número
+//qualquer escolhido pelo usuário até o 10.
+
+let number = prompt("escolha um número: ");
+
+for( let cont =1; cont <=10; cont ++){
+    console.log((cont*number))
+}
+    
+
+//for( let cont =1; cont <=10; cont ++)  -> iforma ate quantos número vc vai multiplicar
+// as duas (()) no cont*number servem para mostrar para o o programa que quem=remos primeiro que ele multiplique e dps exiba o resultado
